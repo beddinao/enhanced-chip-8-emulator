ca9a1706163e3ba02f492dba783406bab69f0f1b
beddinao <bilaleddinaoui@gmail.com>
2025-06-29 01:42:40 +0100

    header

---
+++ include/chip8.h
@@ -0,0 +1,47 @@
+#ifndef CHIP8_H
+#define CHIP8_H
+
+#include <stdio.h>
+#include <stdint.h>
+#include <unistd.h>
+#include <pthread.h>
+#include <SDL3/SDL.h>
+
+#define DEF_WIN_WIDTH 1000
+#define DEF_WIN_HEIGHT 500
+#define MIN_WIN_WIDTH 200
+#define MIN_WIN_HEIGHT 100
+#define RAM_SIZE 4096
+
+
+typedef struct graphic {
+	SDL_Window *window;
+	SDL_Renderer *renderer;
+	unsigned win_height;
+	unsigned win_width;
+	float wpdx;
+	float wpdy;
+} win;
+
+
+typedef struct worker {
+	pthread_t worker;
+	pthread_mutex_t halt_mutex;
+	pthread_mutex_t prg_mutex;
+	bool halt;
+	win *win;
+} worker;
+
+typedef struct chip8 {
+	uint8_t ram[RAM_SIZE];
+	uint8_t regs[16];
+	uint16_t pc;
+	uint16_t ir;
+	uint8_t sp;
+	uint8_t delay_timer;
+	uint8_t sound_timer;
+} chip8;
+
+
+
+#endif
