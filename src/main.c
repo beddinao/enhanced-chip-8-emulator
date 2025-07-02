a268900a4023c0917297dc888abeb46442fc7b34
beddinao <bilaleddinaoui@gmail.com>
2025-07-02 17:40:51 +0100

    modify src/main.c

--- src/main.c
+++ src/main.c
@@ -1,200 +1,91 @@
 #include <chip8.h>
 
-void _0nnn(chip_8 *chip8) { printf("0nnn "); }
-void _00e0(chip_8 *chip8) {
-	//printf("00e0 ");
-	memset(chip8->display, 0, sizeof(chip8->display));
-}
-void _00ee(chip_8 *chip8) {
-	//printf("00ee ");
-	chip8->pc = chip8->stack[chip8->sp--];
-}
-
-void _1nnn(chip_8 *chip8) {
-	//printf("1nnn ");
-	chip8->pc = chip8->opcode&0x0fff;
-}
+void _0nnn(chip_8 *chip8) { chip8->pc = chip8->opcode&0xfff; }
+void _00e0(chip_8 *chip8) { memset(chip8->display, 0, sizeof(chip8->display)); }
+void _00ee(chip_8 *chip8) { if (chip8->sp) chip8->pc = chip8->stack[--chip8->sp]; }
+void _1nnn(chip_8 *chip8) { chip8->pc = chip8->opcode&0xfff; }
 void _2nnn(chip_8 *chip8) {
-	//printf("2nnn ");
-	chip8->stack[++chip8->sp] = chip8->pc;
-	chip8->pc = chip8->opcode&0x0fff;
+	chip8->stack[chip8->sp++] = chip8->pc;
+	chip8->pc = chip8->opcode&0xfff;
 }
-void _3xnn(chip_8 *chip8) {
-	//printf("3xnn ");
-	if (chip8->regs[(chip8->opcode&0x0f00)>>0x8] == chip8->opcode&0xff)
-		chip8->pc += 2;
-}
-void _4xnn(chip_8 *chip8) {
-	//printf("4xnn ");
-	if (chip8->regs[(chip8->opcode&0x0f00)>>0x8] != chip8->opcode&0xff)
-		chip8->pc += 2;
-}
-void _5xy0(chip_8 *chip8) {
-	//printf("5xy0 ");
-	if (chip8->regs[(chip8->opcode&0x0f00)>>0x8] == chip8->regs[(chip8->opcode&0x00f0)>>0x4])
-		chip8->pc += 2;
-}
-void _6xnn(chip_8 *chip8) {
-	//printf("6xnn ");
-	chip8->regs[(chip8->opcode&0x0f00)>>0x8] = chip8->opcode&0xff;
-}
-void _7xnn(chip_8 *chip8) {
-	//printf("7xnn ");
-	chip8->regs[(chip8->opcode&0x0f00)>>0x8] = chip8->regs[(chip8->opcode&0x0f00)>>0x8];
-}
-
-void _8xy0(chip_8 *chip8) {
-	//printf("8xy0 ");
-	chip8->regs[(chip8->opcode&0x0f00)>>0x8] = chip8->regs[(chip8->opcode&0x00f0)>>0x4];
-}
-void _8xy1(chip_8 *chip8) {
-	//printf("8xy1 ");
-	chip8->regs[(chip8->opcode&0x0f00)>>0x8] |= chip8->regs[(chip8->opcode&0x00f0)>>0x4]; 
-}
-void _8xy2(chip_8 *chip8) {
-	//printf("8xy2 ");
-	chip8->regs[(chip8->opcode&0x0f00)>>0x8] &= chip8->regs[(chip8->opcode&0x00f0)>>0x4];
-}
-void _8xy3(chip_8 *chip8) {
-	//printf("8xy3 ");
-	chip8->regs[(chip8->opcode&0x0f00)>>0x8] ^= chip8->regs[(chip8->opcode&0x00f0)>>0x4];
-}
+void _3xnn(chip_8 *chip8) { chip8->pc += chip8->regs[(chip8->opcode&0xf00)>>0x8] == chip8->opcode&0xff ? 2 : 0; }
+void _4xnn(chip_8 *chip8) { chip8->pc += chip8->regs[(chip8->opcode&0xf00)>>0x8] != chip8->opcode&0xff ? 2 : 0; }
+void _5xy0(chip_8 *chip8) { chip8->pc += chip8->regs[(chip8->opcode&0xf00)>>0x8] == chip8->regs[(chip8->opcode&0xf0)>>0x4] ? 2 : 0; }
+void _6xnn(chip_8 *chip8) { chip8->regs[(chip8->opcode&0xf00)>>0x8] = chip8->opcode&0xff; }
+void _7xnn(chip_8 *chip8) { chip8->regs[(chip8->opcode&0xf00)>>0x8] += chip8->opcode&0xff; }
+void _8xy0(chip_8 *chip8) { chip8->regs[(chip8->opcode&0xf00)>>0x8] = chip8->regs[(chip8->opcode&0xf0)>>0x4]; }
+void _8xy1(chip_8 *chip8) { chip8->regs[(chip8->opcode&0xf00)>>0x8] |= chip8->regs[(chip8->opcode&0xf0)>>0x4]; }
+void _8xy2(chip_8 *chip8) { chip8->regs[(chip8->opcode&0xf00)>>0x8] &= chip8->regs[(chip8->opcode&0xf0)>>0x4]; }
+void _8xy3(chip_8 *chip8) { chip8->regs[(chip8->opcode&0xf00)>>0x8] ^= chip8->regs[(chip8->opcode&0xf0)>>0x4]; }
 void _8xy4(chip_8 *chip8) {
-	//printf("8xy4 ");
-	uint16_t res = chip8->regs[(chip8->opcode&0x0f00)>>0x8] + chip8->regs[(chip8->opcode&0x00f0)>>0x4];
+	uint16_t res = chip8->regs[(chip8->opcode&0xf00)>>0x8] + chip8->regs[(chip8->opcode&0xf0)>>0x4];
 	chip8->regs[0xf] = res > 0xff;
-	chip8->regs[(chip8->opcode&0x0f00)>>0x8] = res&0xff;
+	chip8->regs[(chip8->opcode&0xf00)>>0x8] = res&0xff;
 }
 void _8xy5(chip_8 *chip8) {
-	//printf("8xy5 ");
-	chip8->regs[0xf] = chip8->regs[(chip8->opcode&0x0f00)>>0x8] > chip8->regs[(chip8->opcode&0x00f0)>>0x4];
-	chip8->regs[(chip8->opcode&0x0f00)>>0x8] -= chip8->regs[(chip8->opcode&0x00f0)>>0x4];
+	chip8->regs[0xf] = chip8->regs[(chip8->opcode&0xf00)>>0x8] > chip8->regs[(chip8->opcode&0xf0)>>0x4];
+	chip8->regs[(chip8->opcode&0xf00)>>0x8] -= chip8->regs[(chip8->opcode&0xf0)>>0x4];
 }
 void _8xy6(chip_8 *chip8) {
-	//printf("8xy6 ");
-	chip8->regs[0xf] = chip8->regs[(chip8->opcode&0x0f00)>>0x8] & 0x1;
-	chip8->regs[(chip8->opcode&0x0f00)>>0x8] >>= 0x1;
+	chip8->regs[0xf] = chip8->regs[(chip8->opcode&0xf00)>>0x8] & 0x1;
+	chip8->regs[(chip8->opcode&0xf00)>>0x8] >>= 0x1;
 }
 void _8xy7(chip_8 *chip8) {
-	//printf("8xy7 ");
-	chip8->regs[0xf] = chip8->regs[(chip8->opcode&0x00f0)>>0x4] > chip8->regs[(chip8->opcode&0x0f00)>>0x8];
-	chip8->regs[(chip8->opcode&0x0f00)>>0x8] = chip8->regs[(chip8->opcode&0x00f0)>>0x4] - chip8->regs[(chip8->opcode&0x0f00)>>0x8];
+	chip8->regs[0xf] = chip8->regs[(chip8->opcode&0xf0)>>0x4] > chip8->regs[(chip8->opcode&0xf00)>>0x8];
+	chip8->regs[(chip8->opcode&0xf00)>>0x8] = chip8->regs[(chip8->opcode&0xf0)>>0x4] - chip8->regs[(chip8->opcode&0xf00)>>0x8];
 }
 void _8xye(chip_8 *chip8) {
-	//printf("8xye ");
-	chip8->regs[0xf] = chip8->regs[(chip8->opcode&0x0f00)>>0x8] & 0x80;
-	chip8->regs[(chip8->opcode&0x0f00)>>0x8] <<= 0x1;
+	chip8->regs[0xf] = chip8->regs[(chip8->opcode&0xf00)>>0x8] & 0x80;
+	chip8->regs[(chip8->opcode&0xf00)>>0x8] <<= 0x1;
 }
-
-void _9xy0(chip_8 *chip8) {
-	//printf("9xy0 ");
-	if (chip8->regs[(chip8->opcode&0x0f00)>>0x8] != chip8->regs[(chip8->opcode&0x00f0)>>0x4])
-		chip8->pc += 2;
-}
-void _annn(chip_8 *chip8) {
-	//printf("annn ");
-	chip8->ir = chip8->opcode&0x0fff;
-}
-void _bnnn(chip_8 *chip8) {
-	//printf("bnnn ");
-	chip8->pc = chip8->regs[0] + chip8->opcode&0x0fff;
-}
-void _cxnn(chip_8 *chip8) {
-	//printf("cxnn ");
-	chip8->regs[(chip8->opcode&0x0f00)>>0x8] = (rand()%255)&(chip8->opcode&0x00ff);
-}
+void _9xy0(chip_8 *chip8) { chip8->pc += chip8->regs[(chip8->opcode&0xf00)>>0x8] != chip8->regs[(chip8->opcode&0xf0)>>0x4] ? 2 : 0; }
+void _annn(chip_8 *chip8) { chip8->ir = chip8->opcode&0xfff; }
+void _bnnn(chip_8 *chip8) { chip8->pc = chip8->regs[0] + (chip8->opcode&0xfff); }
+void _cxnn(chip_8 *chip8) { chip8->regs[(chip8->opcode&0xf00)>>0x8] = (rand()%255)&(chip8->opcode&0xff); }
 void _dxyn(chip_8 *chip8) {
-	//printf("dxyn ");
-	uint8_t vx = (chip8->opcode&0xf00)>>0x8,
-	vy = (chip8->opcode&0xf0)>>0x4,
-	n = chip8->opcode&0xf;
-	unsigned x = chip8->regs[vx] % 64;
-	unsigned y = chip8->regs[vy] % 32;
-	chip8->regs[0xf] = 0;
-	for (uint8_t row = 0; row < n; row++) {
-		uint8_t sprite_byte = chip8->ram[chip8->ir+row];
+	unsigned x = chip8->regs[(chip8->opcode&0xf00)>>0x8] % 64;
+	unsigned y = chip8->regs[(chip8->opcode&0xf0)>>0x8] % 32;
+	uint8_t sprite_pixel, sprite_byte;
+	uint32_t *display_pixel;
+	for (uint8_t row = 0; row < chip8->opcode&0xf; row++) {
+		sprite_byte = chip8->ram[chip8->ir+row];
 		for (uint8_t col = 0; col < 8; col++) {
-			uint8_t sprite_pixel = (sprite_byte>>(7-col))&0x1;
-			uint32_t *display_pixel = &chip8->display[y+row][x+col];
-			if (*display_pixel == 1 && sprite_pixel == 1)
-				chip8->regs[0xf] = 1;
+			sprite_pixel = (sprite_byte>>(7-col))&0x1;
+			display_pixel = &chip8->display[y+row][x+col];
+			chip8->regs[0xf] = *display_pixel == 1 && sprite_pixel == 1;
 			*display_pixel ^= sprite_pixel;
 		}
 	}
 }
-
 void _ex9e(chip_8 *chip8) { printf("ex9e "); }
 void _exa1(chip_8 *chip8) { printf("exa1 "); }
-
-void _fx07(chip_8 *chip8) {
-	//printf("fx07 ");
-	chip8->regs[(chip8->opcode&0x0f00)>>0x8] = chip8->delay_timer;
-}
+void _fx07(chip_8 *chip8) { chip8->regs[(chip8->opcode&0xf00)>>0x8] = chip8->delay_timer; }
 void _fx0a(chip_8 *chip8) { printf("fx0a "); }
-void _fx15(chip_8 *chip8) {
-	//printf("fx15 ");
-	chip8->delay_timer = chip8->regs[(chip8->opcode&0x0f00)>>0x8];
+void _fx15(chip_8 *chip8) { chip8->delay_timer = chip8->regs[(chip8->opcode&0xf00)>>0x8]; } 
+void _fx18(chip_8 *chip8) { chip8->sound_timer = chip8->regs[(chip8->opcode&0xf00)>>0x8]; }
+void _fx1e(chip_8 *chip8) { chip8->ir += chip8->regs[(chip8->opcode&0xf00)>>0x8]; }
+void _fx29(chip_8 *chip8) { chip8->ir = chip8->regs[(chip8->opcode&0xf00)>>0x8] * 5 + FONT_START; }
+void _fx33(chip_8 *chip8) {
+	chip8->ram[chip8->ir] = chip8->regs[(chip8->opcode&0xf00)>>0x8]/100;
+	chip8->ram[chip8->ir+1] = (chip8->regs[(chip8->opcode&0xf00)>>0x8]/10)%10;
+	chip8->ram[chip8->ir+2] = chip8->regs[(chip8->opcode&0xf00)>>0x8]%10;
 }
-void _fx18(chip_8 *chip8) {
-	//printf("fx18 ");
-	chip8->sound_timer = chip8->regs[(chip8->opcode&0x0f00)>>0x8];
-}
-void _fx1e(chip_8 *chip8) {
-	//printf("fx1e ");
-	chip8->ir += chip8->regs[(chip8->opcode&0x0f00)>>0x8];
-}
-void _fx29(chip_8 *chip8) {
-	//printf("fx29 ");
-	chip8->ir = chip8->regs[(chip8->opcode&0xf00)>>0x8] * 5 + FONT_START;
-}
-void _fx33(chip_8 *chip8) { printf("fx33 "); }
-void _fx55(chip_8 *chip8) {
-	//printf("fx55 ");
-	for (uint8_t reg = 0; reg < (chip8->opcode&0x0f00)>>0x8; reg++)
-		chip8->ram[reg+chip8->ir] = chip8->regs[reg];
-}
-void _fx65(chip_8 *chip8) {
-	//printf("fx65 ");
-	for (uint8_t reg = 0; reg < (chip8->opcode&0x0f00)>>0x8; reg++)
-		chip8->regs[reg] = chip8->ram[reg+chip8->ir];
-}
+void _fx55(chip_8 *chip8) { for (uint8_t reg = 0; reg <= (chip8->opcode&0xf00)>>0x8; reg++) chip8->ram[reg+chip8->ir] = chip8->regs[reg]; }
+void _fx65(chip_8 *chip8) { for (uint8_t reg = 0; reg <= (chip8->opcode&0xf00)>>0x8; reg++) chip8->regs[reg] = chip8->ram[reg+chip8->ir]; }
 
 void load_instructions(chip_8 *chip8) {
-	chip8->_0s_[0] = _0nnn;
-	chip8->_0s_[1] = _00e0;
-	chip8->_0s_[2] = _00ee;
-	chip8->_1_7s_[0] = _1nnn;
-	chip8->_1_7s_[1] = _2nnn;
-	chip8->_1_7s_[2] = _3xnn;
-	chip8->_1_7s_[3] = _4xnn;
-	chip8->_1_7s_[4] = _5xy0;
-	chip8->_1_7s_[5] = _6xnn;
-	chip8->_1_7s_[6] = _7xnn;
-	chip8->_8s_[0] = _8xy0;
-	chip8->_8s_[1] = _8xy1;
-	chip8->_8s_[2] = _8xy2;
-	chip8->_8s_[3] = _8xy3;
-	chip8->_8s_[4] = _8xy4;
-	chip8->_8s_[5] = _8xy5;
-	chip8->_8s_[6] = _8xy6;
-	chip8->_8s_[7] = _8xy7;
-	chip8->_8s_[8] = _8xye;
-	chip8->_9_d_[0] = _9xy0;
-	chip8->_9_d_[1] = _annn;
-	chip8->_9_d_[2] = _bnnn;
-	chip8->_9_d_[3] = _cxnn;
-	chip8->_9_d_[4] = _dxyn;
-	chip8->_es_[0] = _ex9e;
-	chip8->_es_[1] = _exa1;
-	chip8->_fs_[0] = _fx07;
-	chip8->_fs_[1] = _fx0a;
-	chip8->_fs_[2] = _fx15;
-	chip8->_fs_[3] = _fx18;
-	chip8->_fs_[4] = _fx1e;
-	chip8->_fs_[5] = _fx29;
-	chip8->_fs_[6] = _fx33;
-	chip8->_fs_[7] = _fx55;
-	chip8->_fs_[8] = _fx65;
+	chip8->_0s_[0] = _0nnn; chip8->_0s_[1] = _00e0; chip8->_0s_[2] = _00ee;
+	chip8->_1_7s_[0] = _1nnn; chip8->_1_7s_[1] = _2nnn; chip8->_1_7s_[2] = _3xnn;
+	chip8->_1_7s_[3] = _4xnn; chip8->_1_7s_[4] = _5xy0; chip8->_1_7s_[5] = _6xnn;
+	chip8->_1_7s_[6] = _7xnn; chip8->_8s_[0] = _8xy0; chip8->_8s_[1] = _8xy1;
+	chip8->_8s_[2] = _8xy2; chip8->_8s_[3] = _8xy3; chip8->_8s_[4] = _8xy4;
+	chip8->_8s_[5] = _8xy5; chip8->_8s_[6] = _8xy6; chip8->_8s_[7] = _8xy7;
+	chip8->_8s_[8] = _8xye; chip8->_9_d_[0] = _9xy0; chip8->_9_d_[1] = _annn;
+	chip8->_9_d_[2] = _bnnn; chip8->_9_d_[3] = _cxnn; chip8->_9_d_[4] = _dxyn;
+	chip8->_es_[0] = _ex9e; chip8->_es_[1] = _exa1; chip8->_fs_[0] = _fx07;
+	chip8->_fs_[1] = _fx0a; chip8->_fs_[2] = _fx15; chip8->_fs_[3] = _fx18;
+	chip8->_fs_[4] = _fx1e; chip8->_fs_[5] = _fx29; chip8->_fs_[6] = _fx33;
+	chip8->_fs_[7] = _fx55; chip8->_fs_[8] = _fx65;
 }
 
 void load_fonts(chip_8 *chip8) {
@@ -229,19 +120,23 @@ bool init_window(worker_data *worker) {
 	worker->win->win_width = DEF_WIN_WIDTH;
 	worker->win->ppy = worker->win->win_height/32.0;
 	worker->win->ppx = worker->win->win_width/64.0;
-	//printf("ppx: %f, ppy: %f\n", worker->win->ppx, worker->win->ppy);
 	if (!SDL_Init(SDL_INIT_EVENTS)) {
 		free(worker->win);
 		return false;
 	}
-	win = SDL_CreateWindow("chip 8 emu", worker->win->win_width, worker->win->win_height, SDL_WINDOW_RESIZABLE);
+	win = SDL_CreateWindow("chip 8 emu",
+		worker->win->win_width,
+		worker->win->win_height,
+		SDL_WINDOW_RESIZABLE);
 	renderer = SDL_CreateRenderer(win, NULL);
 	if (!win || !renderer) {
 		if (win) SDL_DestroyWindow(win);
 		free(worker->win);
 		return false;
 	}
-	SDL_SetWindowMinimumSize(win, MIN_WIN_WIDTH, MIN_WIN_HEIGHT);
+	SDL_SetWindowMinimumSize(win,
+		MIN_WIN_WIDTH,
+		MIN_WIN_HEIGHT);
 	worker->win->window = win;
 	worker->win->renderer = renderer;
 	return true;
@@ -351,6 +246,7 @@ void *instruction_cycle(void *p) {
 	bool emu_on = true;
 	worker_data *worker = (worker_data*)p;
 	chip_8 *chip8 = worker->chip8;
+	uint8_t n;
 	while (emu_on) {
 		pthread_mutex_lock(&worker->halt_mutex);
 		if (worker->halt) {
@@ -362,8 +258,6 @@ void *instruction_cycle(void *p) {
 			break;	
 		chip8->opcode = chip8->ram[chip8->pc] << 0x8 | chip8->ram[chip8->pc+1];
 		chip8->pc += 2;
-		////printf("%02x ", (chip8->opcode & 0xF000) >> 12);
-		uint8_t n;
 		switch ((chip8->opcode & 0xF000) >> 12) {
 			case 0x0:
 				if (chip8->opcode == 0x00e0) chip8->_0s_[1](chip8);
